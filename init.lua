29539b99d4e8430e2fa58966541c3b195e9394c6
seaworthybus <56329690+SeaworthyBus@users.noreply.github.com>
2026-01-09 16:05:00 -0500

    neorg update

--- init.lua
+++ init.lua
@@ -284,12 +284,29 @@ require('lazy').setup({
       },
     },
   },
-
   {
     'nvim-neorg/neorg',
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = '*', -- Pin Neorg to the latest stable release
-    config = true,
+    lazy = false,
+    version = '*',
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
